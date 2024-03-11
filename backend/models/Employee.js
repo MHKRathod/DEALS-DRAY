eca9cf71584d1406d7fd4183d292a6caeb5b5f57
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-03-11 19:41:57 +0530

    added create date in schema

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  mobile: { type: String, required: true },
-  designation: { type: String, required: true },
-  gender: {
-    type: String,
-    enum: ['Male', 'Female', 'Other'] // Define enum values here
-},
-  courses: [{ type: String }],
-  image: { type: String }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    mobile: { type: String, required: true },
+    designation: { type: String, required: true },
+    gender: { type: String, required: true },
+    courses: [{ type: String }],
+    img: { type: String }, // Assuming the image URL is stored as a string
+    createDate: { type: Date, default: Date.now } // Automatically set to the current date and time when a new document is created
 });
 
-module.exports = mongoose.model('Employee', employeeSchema);
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
