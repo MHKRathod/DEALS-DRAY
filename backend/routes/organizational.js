36726fabdd5d82fb07394b5e02967954386e9cf8
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-04-21 23:39:27 +0530

    subordinate and manager api

--- backend/routes/organizational.js
+++ backend/routes/organizational.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Employee = require('../models/Employee');
 
 const {organizationalStructureHandler,getSubordinateHandler} = require('../controllers/organizationalController');
-
-
 
 router.get('/organizational', organizationalStructureHandler);
 
 router.get('/subordinates/:employeeId', getSubordinateHandler);
+
+router.get('/employeesandmanager/:employeeId', async (req, res) => {
+    const { employeeId } = req.params;
+    try {
+        const employee = await Employee.findById(employeeId)
+            .populate('manager') // Populate the manager field with details
+            .exec();
+        
+        if (!employee) {
+            return res.status(404).json({ error: "Employee not found" });
+        }
+        
+        res.json(employee);
+    } catch (error) {
+        console.error('Error fetching employee:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 module.exports = router;
 
